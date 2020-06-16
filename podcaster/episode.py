bb6b7d5237604e04994591a8b85dfeada1cdb5d5
Niklas <niklas.mertsch@stud.uni-goettingen.de>
2020-06-16 23:52:43 +0200

    Added support for heise podcasts: 1. removed subtitle parsing (was never used anyway) 2. added a second possible url path in RSS format (default: link, fallback: links[0]["href"])

--- podcaster/episode.py
+++ podcaster/episode.py
@@ -6,11 +6,12 @@ from time import mktime
 class Episode:
     def __init__(self, feed_entry):
         self.rss = feed_entry
-        self.url = feed_entry.link
-        self.title = feed_entry.title
-        self.subtitle = feed_entry.subtitle
-        self.summary = feed_entry.summary
         self.date = datetime.fromtimestamp(mktime(feed_entry.published_parsed))
+        self.title = feed_entry.title
+        try:
+            self.url = feed_entry.link
+        except AttributeError:
+            self.url = feed_entry.links[0]["href"]
 
     def __str__(self):
         return self.title
