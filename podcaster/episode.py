36b9ac833a98e8c61df88192cace3683f1a007f0
Niklas <niklas.mertsch@stud.uni-goettingen.de>
2020-06-20 18:04:15 +0200

    Added type hint to Episode rss

--- podcaster/episode.py
+++ podcaster/episode.py
@@ -2,9 +2,11 @@ from datetime import datetime
 import os
 from time import mktime
 
+from feedparser import FeedParserDict
+
 
 class Episode:
-    def __init__(self, feed_entry):
+    def __init__(self, feed_entry: FeedParserDict):
         self.rss = feed_entry
         self.date = datetime.fromtimestamp(mktime(feed_entry.published_parsed))
         self.title = feed_entry.title
